5d2844d8d6d525133b5317d0531cb530e54e40b2
Kamil Mingaliev <kamil.mingaliev16@mail.ru>
2026-01-18 01:07:21 +0300

    Познакомился с input и с переменными

--- hello_project/HelloWorld.py
+++ hello_project/HelloWorld.py
@@ -1,7 +1,24 @@
+'''
 print("HelloWorld!\n")
 print("KAMIL itis")
+'''
 # Комментарий
 '''
 Многострочный комментарий
 !!!
 '''
+
+word: str = "Результат"
+number: int = 42
+bool = True
+num2: float = 3.1415
+number -=2
+
+a = b = c = 10
+a += 5
+w,e,r = "Word",2,3
+
+print(word, w,e)
+
+age = input("Введите что-нибудь: ")
+print("Вы ввели:", age)
