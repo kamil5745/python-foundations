4028fd4235e3a363ac98bd9fb4004399161f217f
Kamil Mingaliev <kamil.mingaliev16@mail.ru>
2026-01-18 01:37:08 +0300

    создали калькулятор

---
+++ hello_project/calculator.py
@@ -0,0 +1,11 @@
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+
+sum = a + b
+minus = a - b
+mult = a * b
+delenie = a / b
+print("Результат сложения:", sum)
+print("Результат вычитания:", minus)
+print("Результат умножения:", mult)
+print("Результат деления:", delenie)
