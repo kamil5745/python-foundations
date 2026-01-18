75dccf9c76a9f231e346a7dffbddc31fa8aaeca6
Kamil Mingaliev <kamil.mingaliev16@mail.ru>
2026-01-18 10:24:40 +0300

    Выполнил практику 1

--- Practice_1/base.py
+++ Practice_1/base.py
@@ -1,10 +1,40 @@
+'''
 a = '\''
 b = '"'
 c = '" \''
 
-d = r''' \
-	
-''' '''
-	Hello\n
-'''
+d = '\n\t\\'
 print(d)
+'''
+'''
+s = "backend"
+print(s[0],s[6], s[3], s[-2])
+
+s = "programming"
+print(s[:3],s[3:7],s[7:11],s[2:],s[:-2])
+
+word = "python"
+word = "P" + word[1:]
+print(word)
+
+ha = 2*("ha"+"-") + "ha!"
+print(ha)
+'''
+
+a = ("Python is sim"
+"ple but powerful")
+print(a)
+
+user = "kamil"
+action = "login"
+
+print(f"User {user} performed action: {action}")
+print("User "+user+ " performed action: "+action)
+
+s = r"C:\users\kamil\projects\python"
+r = "C:\\users\\kamil\\projects\\python"
+print(s,r)
+
+
+
+
