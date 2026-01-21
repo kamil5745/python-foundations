fefa4d514cbf84c83189f7ed51744265409e5ae5
Kamil Mingaliev <kamil.mingaliev16@mail.ru>
2026-01-21 21:02:43 +0300

    Сделал часть практики Data Structure

--- Practice_1/practice_ds_ch5.py
+++ Practice_1/practice_ds_ch5.py
@@ -47,6 +47,83 @@ def normalize_tags(raw: list[str]) -> list[str]:
 	return result
 print(normalize_tags(raw))
 
+'''
+'''
+lst = list()
+lst.append('a')
+lst.append('b')
+lst.append('c')
+#lst.extend("abc")
+lst.pop()
+lst.pop()
+print(lst)
+
+'''
+def is_balanced(s: str) -> bool:
+	if len(s) == 0:
+		return True
+	if len(s)%2 != 0 or s[0] == ")" or s[len(s)-1] == "(":
+		return False
+	pair_scob = 0
+	for now_scob in s:
+		if now_scob == '(':
+			pair_scob += 1
+		if now_scob == ')':
+			pair_scob -= 1
+	if pair_scob == 0:
+		return True
+	else:
+		return False
+'''
+def is_balanced(s: str) -> bool:
+	stack = []
+	for el in s:
+		if el == "(":
+			stack.append(el)
+		elif el == ')':
+			if not stack:
+				return False
+			stack.pop()
+	return not stack
+print(is_balanced("()))((()"))
+
+lst = [1,2,3,"4", "5", '6', True, []]
+print(lst)
+print(lst.pop())
+print(lst)
+print(lst.pop(0))
+print(lst)
+# при pop() удаляется последний элемент и возвращатся pop, при это эл-ты списка не переписывается
+# при pop(0) удаляется ссылка - первый элемент из списка и остальные ссылки элементы списка все переписываются на предыдущий индекс
+
+
+history = ['']
+
+def type_text(t: str) -> None:
+	history.append(history[-1] + t)
+
+def undo() -> None:
+	if len(history) > 1:
+		history.pop()
+
+def print_text() -> None:
+	print(history[-1])
+
+type_text("Hello")
+print_text()        # Hello
+type_text(" world")
+print_text()        # Hello world
+undo()
+print_text()        # Hello
+print(history)
+
+
+
+
+
+
+
+
 
 
 
